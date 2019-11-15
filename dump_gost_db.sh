62e2183c2415f0e4da672f96f843959c4bfa1b01
Acoucite <51779890+Acoucite@users.noreply.github.com>
2019-11-15 12:06:33 +0100

    V1
    Some corrections

--- dump_gost_db.sh
+++ dump_gost_db.sh
@@ -10,7 +10,7 @@ echo "$GOST_DB_HOST:$GOST_DB_PORT:$GOST_DB:$GOST_DB_USER:$GOST_DB_PASSWORD" > ~/
 chmod 0600 ~/.pgpass
 
 pg_dump --no-acl -h $GOST_DB_HOST -T v1.observation -p $GOST_DB_PORT -U $GOST_DB_USER $GOST_DB > /monica/dumps/gost.sql
-pg_dump -h $GOST_DB_HOST -p $GOST_DB_PORT -U $GOST_DB_USER $GOST_DB -t v1.observation -s > /monica/dumps/observation.sql
+pg_dump --no-acl -h $GOST_DB_HOST -p $GOST_DB_PORT -U $GOST_DB_USER $GOST_DB -t v1.observation -s > /monica/dumps/observation.sql
 psql -h $GOST_DB_HOST -p $GOST_DB_PORT -U $GOST_DB_USER $GOST_DB -c "\copy (SELECT * FROM v1.observation WHERE (stream_id IN (20220,20311) OR stream_id=129) AND data ->> 'phenomenonTime' BETWEEN '2019-08-30T21:00:00.000Z' AND '2019-08-30T22:00:00.000Z') TO '/monica/dumps/obs.csv' with delimiter as ','"
 
 
